0ca58235ffec0b7c5b8b88d798e072bde0a5e7b3
krystiano13 <crystianotv@gmail.com>
2024-04-22 13:45:37 +0200

    add react router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,47 +1,14 @@
-import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { Home } from "./views/Home.tsx";
 
 function App() {
-    const [count, setCount] = useState(0);
-
-    useEffect(() => {
-        const formData = new FormData();
-        formData.append("email", "test@example.com");
-        formData.append("password", "password");
-
-        fetch("http://127.0.0.1:3000/users/tokens/sign_in", {
-            method: "POST",
-            body: formData
-        })
-            .then(res => res.json())
-            .then(data => console.log(data))
-    }, []);
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Home />} />
+        </Routes>
+    </BrowserRouter>
   )
 }
 
