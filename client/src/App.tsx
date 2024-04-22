d2611216284aca4acecb907e777eab871ca4d243
krystiano13 <crystianotv@gmail.com>
2024-04-22 14:13:36 +0200

    create logged and not logged state for navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
 
 import { Login } from "./views/Login.tsx";
 import { Register } from "./views/Register.tsx";
@@ -6,16 +7,17 @@ import { Navbar } from "./components/Navbar.tsx";
 import "./App.css"
 
 function App() {
-  return (
-    <main className="w-[100vw] h-[100vh] bg-gray-800 flex flex-col">
-        <BrowserRouter>
-            <Navbar />
-            <Routes>
-                <Route path="/" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-            </Routes>
-        </BrowserRouter>
-    </main>
+    const [loggedIn, setLoggedIn] = useState<boolean>(false);
+    return (
+        <main className="w-[100vw] h-[100vh] bg-gray-800 flex flex-col">
+            <BrowserRouter>
+                <Navbar loggedIn={loggedIn} />
+                <Routes>
+                    <Route path="/" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                </Routes>
+            </BrowserRouter>
+        </main>
   )
 }
 
